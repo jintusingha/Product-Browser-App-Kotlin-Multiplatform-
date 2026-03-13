35d432e971dcef4f7d10917cf86f1159b4b7ca60
jintu singha <jintumonisingha411@gmail.com>
2026-03-13 15:36:53 +0530

    Added Domain product model

---
+++ composeApp/src/commonMain/kotlin/org/example/project/domain/model/Product.kt
@@ -0,0 +1,11 @@
+package org.example.project.domain.model
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Double,
+    val brand: String?,
+    val rating: Double,
+    val thumbnail: String,
+)
