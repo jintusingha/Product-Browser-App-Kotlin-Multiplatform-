1afd40ddaa021e2e9a5d4af924a66663f67a8702
jintu singha <jintumonisingha411@gmail.com>
2026-03-13 15:33:48 +0530

    Added product Dto API response

---
+++ composeApp/src/commonMain/kotlin/org/example/project/data/dto/ProductDto.kt
@@ -0,0 +1,14 @@
+package org.example.project.data.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProductDto(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Double,
+    val brand: String? = null,
+    val rating: Double,
+    val thumbnail: String,
+)
